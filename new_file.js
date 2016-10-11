0f25a3dcbb5f6f36a946cfc386ac814ea0521b6c
wangjie4409 <1287798820@qq.com>
2016-10-11 19:17:58 +0800

    Add files via upload

---
+++ new_file.js
@@ -0,0 +1,55 @@
+$(function(){
+	(function(){
+		var curr = 0;
+		$("#jsNav .trigger").each(function(i){
+			$(this).click(function(){
+				curr = i;
+				$("#js img").eq(i).fadeIn("slow").siblings("img").hide();
+				$(this).siblings(".trigger").removeClass("imgSelected").end().addClass("imgSelected");
+				return false;
+			});
+		});
+		
+		var pg = function(flag){
+			if (flag) {
+				if (curr == 0) {
+					todo = 2;
+				} else {
+					todo = (curr - 1) % 3;
+				}
+			} else {
+				todo = (curr + 1) % 3;
+			}
+			$("#jsNav .trigger").eq(todo).click();
+		};
+		
+		//ǰ
+		$("#prev").click(function(){
+			pg(true);
+			return false;
+		});
+		
+		//
+		$("#next").click(function(){
+			pg(false);
+			return false;
+		});
+		
+		//Զ
+		var timer = setInterval(function(){
+			todo = (curr + 1) % 3;
+			$("#jsNav .trigger").eq(todo).click();
+		},2000);
+		
+		$("#js,#prev,#next").hover(function(){
+				clearInterval(timer);
+			},
+			function(){
+				timer = setInterval(function(){
+					todo = (curr + 1) % 3;
+					$("#jsNav .trigger").eq(todo).click();
+				},2000);			
+			}
+		);
+	})();
+});
